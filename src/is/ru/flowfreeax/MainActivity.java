bad9c4177f650d0c4211c7d77c753cc187c94bb7
Joddsson <ivaroddsson@gmail.com>
2014-09-10 17:27:10 +0000

    Added an image to the home screen.

--- src/is/ru/flowfreeax/MainActivity.java
+++ src/is/ru/flowfreeax/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
     /**
@@ -14,6 +15,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        ImageView image = (ImageView) findViewById(R.id.mainImgage);
     }
 
     public void buttonClick(View view){
