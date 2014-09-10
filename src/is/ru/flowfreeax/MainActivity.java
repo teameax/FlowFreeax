24516e138cf9bf641fedaed479c098253cdf6823
Joddsson <ivaroddsson@gmail.com>
2014-09-10 19:23:39 +0000

    Fixed the button sizes on home sceen

--- src/is/ru/flowfreeax/MainActivity.java
+++ src/is/ru/flowfreeax/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
@@ -20,6 +21,7 @@ public class MainActivity extends Activity {
 
     public void buttonClick(View view){
         Button button = (Button) view;
+
         int id = button.getId();
         if(id == R.id.button_play){
             startActivity(new Intent(this, PlayActivity.class));
