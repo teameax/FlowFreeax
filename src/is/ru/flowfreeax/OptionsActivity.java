e320b3f61bd7ce4f070fa71fb4e92d52ececc198
Joddsson <ivaroddsson@gmail.com>
2014-09-15 15:18:58 +0000

    Mini fixes and added structure to the options activty.

--- src/is/ru/flowfreeax/OptionsActivity.java
+++ src/is/ru/flowfreeax/OptionsActivity.java
@@ -1,10 +1,15 @@
 package is.ru.flowfreeax;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by joddsson on 15.9.2014.
  */
 public class OptionsActivity extends Activity{
-
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.options);
+    }
 }
